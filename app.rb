89566c237996b51b48da92f107beca4629959bde
Nick Rupp <nickrupp95@gmail.com>
2017-03-28 11:05:10 +0100

    Add param option to named cat route

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 require 'sinatra'
 
-get "/cat" do
+get "/random-cat" do
   @name = ["Cortez", "Franceso", "Pablo", "Xenith", "Petunia", "Ferndandez", "Euge"].sample
+  erb(:index)
+end
+
+get "/named-cat" do
+  p params
+  @name = params[:name]
   erb(:index)
 end
