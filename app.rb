fb4c0d91bf9f09b7d79c7489f9fe564420b4922e
Nick Rupp <nickrupp95@gmail.com>
2017-03-28 12:20:43 +0100

    Extracted Name Form and changed from GET to POST

--- app.rb
+++ app.rb
@@ -1,11 +1,19 @@
 require 'sinatra'
 
-get "/random-cat" do
+get '/' do
+  erb(:index)
+end
+
+get '/random-cat' do
   @name = ["Cortez", "Franceso", "Pablo", "Xenith", "Petunia", "Ferndandez", "Euge"].sample
   erb(:index)
 end
 
-get "/named-cat" do
+get '/name-form' do
+  erb("name-form".to_sym)
+end
+
+post '/named-cat' do
   p params
   @name = params[:name]
   erb(:index)
