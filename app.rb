c1c17907efd5cd37c6f0c3b65a9280c917f64180
Nick Rupp <nickrupp95@gmail.com>
2017-03-28 10:38:52 +0100

    Separate views from index

--- app.rb
+++ app.rb
@@ -2,10 +2,5 @@ require 'sinatra'
 
 get "/cat" do
 
-  "<div style='border: medium dashed red'>
-  <img src='http://bit.ly/1eze8aE'>
-  <img src='http://bit.ly/1eze8aE'>
-  <img src='http://bit.ly/1eze8aE'>
-  <iframe src='https://gfycat.com/ifr/WarmheartedAnyFairyfly' frameborder='0' scrolling='no' width='640' height='492' allowfullscreen></iframe>
-  </div>"
+  erb(:index)
 end
